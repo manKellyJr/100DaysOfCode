1bf819eab9f01ac7614039d0f393aacea370066d
Obed Banda <obedbanda34@gmail.com>
2022-10-05 21:44:26 +0200

    Recording peoples favorite numbers

---
+++ round_one/004/favorite_numbers.py
@@ -0,0 +1,16 @@
+# favorite numbers
+favorite_numbers = {
+    'obed' : 7,
+    'john' : 3,
+    'zondile' : 4,
+    'agness' : 5,
+    'dole' : 2,
+    'luwi' : 9
+    }
+
+print('Obed\'s favorite number is ' + str(favorite_numbers['obed']))
+print('John\'s favorite number is ' + str(favorite_numbers['john']))
+print('Zondile\'s favorite number is ' + str(favorite_numbers['zondile']))
+print('Agness\'s favorite number is ' + str(favorite_numbers['agness']))
+print('Dole\'s favorite number is ' + str(favorite_numbers['dole']))
+print('Luwi\'s favorite number is ' + str(favorite_numbers['luwi']))
