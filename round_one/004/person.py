dbfe0cfed24c45e2222aa7a91416559b127b5494
Obed Banda <obedbanda34@gmail.com>
2022-10-05 21:46:06 +0200

    Record a person's details

---
+++ round_one/004/person.py
@@ -0,0 +1,13 @@
+#a persons deta using a dictionary
+
+person = {
+    'first_name' : 'John',
+    'last_name' : 'Doe',
+    'age' : 24,
+    'city' : 'kitwe'    
+    }
+#printing each piece of information
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
