cf316961e2fd5b7c678e54a81a0182f63e8bf80f
Obed Banda <obedbanda34@gmail.com>
2022-10-07 06:04:42 +0200

    Function and function parameters and arguments

---
+++ round_one/006/t_shirt.py
@@ -0,0 +1,6 @@
+# T-Shirt function
+def t_shirt(size, message):
+     """Function that prints that T-shirt size and the message to be writen on the T-Shirt"""
+     print("Size: " + size.upper() + "\nMessage" + message.title())
+
+t_shirt('l', "H.T.M.L")
