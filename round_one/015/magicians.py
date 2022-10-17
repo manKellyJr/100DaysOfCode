5165463171807f2bf2f342b2289790ad5021ff6d
Obed Banda <obedbanda34@gmail.com>
2022-10-17 05:02:23 +0200

    showing Magicians name list

---
+++ round_one/015/magicians.py
@@ -0,0 +1,9 @@
+def show_magicians(magicians):
+    """A function tha print a list of names of magicians"""
+    for name in magicians:
+        print(name.title())
+
+
+list_of_magicians = ['obed', 'kelly', 'jr', 'banda']
+
+show_magicians(list_of_magicians)
