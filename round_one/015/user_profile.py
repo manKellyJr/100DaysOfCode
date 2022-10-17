36bd935e167592c83f010917ab278b186fa2f8f3
Obed Banda <obedbanda34@gmail.com>
2022-10-17 22:19:34 +0200

    User Profile details using arbitrary arguments

---
+++ round_one/015/user_profile.py
@@ -0,0 +1,13 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    profile = {'first_name': first, 'last_name': last}
+    for key, value in user_info.items():
+        profile[key] = value
+        return profile
+
+
+user_profile = build_profile('abelt', 'einstein',
+                             location='priceton',
+                             field='physics')
+
+print(user_profile)
