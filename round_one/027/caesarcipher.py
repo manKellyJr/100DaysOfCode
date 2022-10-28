f72caa211c9e98c1fe27788fbfbc9413500d4f0c
Obed Banda <obedbanda34@gmail.com>
2022-10-28 23:35:44 +0200

    Caesar Cipher completed

--- round_one/027/caesarcipher.py
+++ round_one/027/caesarcipher.py
@@ -4,11 +4,12 @@ to encrypt and decrypt letters.
 
 
 Tags: short, beginner, cryptography, math"""
+import pyperclip
 
 try:
-    import pyperclip    # pyperclip copies text to the clipboard.
+    import pyperclip  # pyperclip copies text to the clipboard.
 except ImportError:
-    pass    # If pyperclip is not installed, do nothing. Its no big deal.
+    pass  # If pyperclip is not installed, do nothing. Its no big deal.
 
 # Every possible symbol that  can be encrypted/decrypted:
 # (!) You can add numbers and punctuationmarks to encrypt those
@@ -33,6 +34,7 @@ while True:  # Keep asking until the user enters  e or d.
         break
     print('Please enter the e or d')
 
+# Let the user enter the key to use:
 while True:  # Keep asking until the user enters a valid key.
     maxKey = len(SYMBOLS) - 1
     print('Please enter the key (0 to {}) to use.'.format(maxKey))
@@ -44,7 +46,41 @@ while True:  # Keep asking until the user enters a valid key.
         key = int(response)
         break
 
+# Let the user enter the message to encrypt/decryp:
+print('Enter the message to {}.'.format(mode))
+message = input('> ')
 
+# Caesar cipher only works on uppercase letters:
+message = message.upper()
 
+# Store the encrypted/decrypted form of the message:
+translated = ''
 
+# Encrypt/decrypt each of the symbol in the message:
+for symbol in message:
+    if symbol in SYMBOLS:
+        # Get the encrypted (or decrypted) number for this symbol.
+        num = SYMBOLS.find(symbol)  # Get the number of the symbol
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
 
+        # Handle the wrap-around if num is larger than the length of
+        # SYMBOLS or less than 0:
+        if num >= len(SYMBOLS):
+            num = num - len(SYMBOLS)
+        elif num < 0:
+            num = num + len(SYMBOLS)
+
+        # Add encrypted/decrypted number's symbol to translated
+        translated = translated + SYMBOLS[num]
+
+# Display the encrypted/decrypted string to the screen
+print(translated)
+
+try:
+    pyperclip.copy(translated)
+    print('Full {}ed text tex copied to clipboard.'.format(mode))
+except:
+    pass  # Do nothing if pyperclip wasn't installed.
