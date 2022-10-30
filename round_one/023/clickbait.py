b8ca84f9f19885293e5fce30fbd1c686197e4e8e
Obed Banda <obedbanda34@gmail.com>
2022-10-30 23:30:33 +0200

    Clickbait done

--- round_one/023/clickbait.py
+++ round_one/023/clickbait.py
@@ -6,11 +6,11 @@ Tags: large, beginner, humor, word"""
 import random
 
 # Set up the constants
-OBJBECT_PRONOUNS = ['Her', 'Him', 'Them']
+OBJECT_PRONOUNS = ['Her', 'Him', 'Them']
 POSSESIVE_PRONOUNS = ['Her', 'His', 'Their']
 PERSONAL_PRONOUNS = ['She', 'He', 'They']
 STATES = ['California', 'Texa', 'Lusaka', 'Kitwe', 'Ndola', 'Ohio',
-          'Chingola', 'Kabwe', 'Choma', 'Livingstonr', 'Chilanga']
+          'Chingola', 'Kabwe', 'Choma', 'Livingstone', 'Chilanga']
 NOUNS = ['Athlete', 'Clown', 'Shovel', 'Paleo Diet', 'Doctor', 'Parent',
          'Cat', 'Dog', 'Chicken', 'Robot', 'Video Game', 'Avocado',
          'Plastic straw', 'Serial Killer', 'Telephone Psychic']
@@ -42,15 +42,15 @@ def main():
         elif clickbaitType == 2:
             headline = generateWhatYouDontKnowHeadline()
         elif clickbaitType == 3:
-            headline = generateBigCompaniesHateHerHeadlin()
+            headline = generateBigCompaniesHateHerHeadline()
         elif clickbaitType == 4:
             headline = generateYouWontBelieveHeadline()
         elif clickbaitType == 5:
             headline = generateDontWantYouToKnowHeadline()
         elif clickbaitType == 6:
-            headline = generateGiftIdeaWhyHeadline()
+            headline = generateGiftIdeaHeadline()
         elif clickbaitType == 7:
-            headline = generateReadonWhyHeadline()
+            headline = generateReasonsWhyHeadline()
         elif clickbaitType == 8:
             headline = generateJobAutomatedHeadline()
 
@@ -66,4 +66,66 @@ def main():
 # Each of these functions returns a different type of headline:
 def generateAreMillennialsKillingHeadline():
     noun = random.choice(NOUNS)
-    return 'Are Millennials Killing the {} Industry?'.format
+    return 'Are Millennials Killing the {} Industry?'.format(noun)
+
+
+def generateWhatYouDontKnowHeadline():
+    noun = random.choice(NOUNS)
+    pluralNoun = random.choice(NOUNS) + 's'
+    when = random.choice(WHEN)
+    return 'Without This {}, {} Could Kill Your'.format(noun, pluralNoun, when)
+
+
+def generateBigCompaniesHateHerHeadline():
+    pronoun = random.choice(OBJECT_PRONOUNS)
+    state = random.choice(STATES)
+    noun1 = random.choice(NOUNS)
+    noun2 = random.choice(NOUNS)
+    return 'Big Companies Hate {}! See How This {} {} Invented a Cheaper {}'.\
+        format(pronoun, state, noun1, noun2)
+
+def generateYouWontBelieveHeadline():
+    state = random.choice(STATES)
+    noun = random.choice(NOUNS)
+    pronoun = random.choice(POSSESIVE_PRONOUNS)
+    place = random.choice(PLACES)
+    return 'You Won\'t Believe What This {} {} Found in {} {}'.format(state, noun,
+                                                                      pronoun, place)
+
+def generateDontWantYouToKnowHeadline():
+    pluralNoun1 = random.choice(NOUNS) + 's'
+    pluralNoun2 = random.choice(NOUNS) + 's'
+    return 'What {} Don\'t What You to Know About {}'.format(pluralNoun1, pluralNoun2)
+
+
+def generateGiftIdeaHeadline():
+    number = random.randint(7, 15)
+    noun = random.choice(NOUNS)
+    state = random.choice(STATES)
+    return '{} Gift Ideas to Give Your {} From {}'.format(number, noun, state)
+
+
+def generateReasonsWhyHeadline():
+    number1 = random.randint(3, 19)
+    pluralNoun = random.choice(NOUNS) + 's'
+    # number2 should be no larger than number1:
+    number2 = random.randint(1, number1)
+    return '{} Reasons Why {} Are More Interesting than You Think (Number {} Will Surprise You!)'.format(number1, pluralNoun, number2)
+    
+
+def generateJobAutomatedHeadline():
+    state = random.choice(STATES)
+    noun = random.choice(NOUNS)
+    
+    i = random.randint(0, 2)
+    pronoun1 = POSSESIVE_PRONOUNS[i]
+    pronoun2 = PERSONAL_PRONOUNS[i]
+    if pronoun1 == 'Their':
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Were Wrong.'.format(state, noun, pronoun1, pronoun2)
+    else:
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Was Wrong.'.format(state, noun, pronoun1, pronoun2)
+
+
+# If the prograam is run (instead of imported), run the game:
+if __name__ == '__main__':
+    main()
