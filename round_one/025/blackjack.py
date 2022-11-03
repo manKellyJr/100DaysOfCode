3d3ae8dc6e36222bd4f9a2d82bf9419a6255320e
Obed Banda <obedbanda34@gmail.com>
2022-11-03 18:25:35 +0200

    This program is the longest, currently on line 208

--- round_one/025/blackjack.py
+++ round_one/025/blackjack.py
@@ -111,3 +111,99 @@ def main():
             money -= bet
         elif playerValue > dealerValue:
             print('You won ${}!'.format(bet))
+            money += bet
+        elif playerValue == dealerValue:
+            print('It\'s a tie, the bet is returned to you.')
+
+        input('Press Enter to continue...')
+        print('\n\n')
+
+
+def getBet(maxBet):
+    """Ask the player how much they want to bet for this ground."""
+    while True: # Keep asking until they enter a valid amount.
+        print('How much do you want to bet? (1-{}, or QUIT'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thank you for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue    # If the player didn't enter a number, ask again
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet  # Player entered a valid bet.
+
+
+def getDeck():
+    """Return a list of (rank, suit) tuples for all 52 cards."""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            deck.append((str(rank), suit))    # Add the numbered cards.
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))   # Add the face and ace cards.
+        random.shuffle(deck)
+        return deck
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if showDealer is False"""
+    print()
+    if showDealerHand:
+        print('DEALER: ', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        displayCards([BACKSIDE] + dealerHand[:1])
+
+    # Show the player's cards:
+    print('PLAYER:', getHandValue(playerHand))
+    displayCards(playerHand)
+
+
+def getHandBalue(cards):
+    """Returns the value of the cards. Face cards are woth 10, aces are
+    worth 11 0r 1 (this function picks the most suitable ace value)."""
+    value = 0
+    numberOfAces = 0
+
+    # Add the value for the non-ace cards:
+    for card in cards:
+        rank = card[o0] # card is a tuple like (rank, suit)
+        if rank == 'A':
+            numberOfAces += 1
+        elif rank in ('K', 'Q', 'J'):   # Face cards are worth 10 points
+            value += 10
+        else:
+            value += int(rank)  # Numbered cards are worth their number.
+
+    # Add the value for the aces:
+    value += numberOfAces   # Add 1 per ace.
+    for i in range(numberOfAces):
+        # If another 10 can be added with busting, do so:
+        if value + 10 < 21:
+            value += 10
+
+    return value
+
+
+def displayCards(cards):
+    """Display all the cards in the cards list."""
+    rows = ['', '', '', '', ''] # The text to dis[;ay on each row.
+
+    for i, card in enumerate(cards):
+        rows[0] += ' ____ ' # Print the top line of the card.
+        if card == BACKSIDE:
+            # Print a card's back:
+            rows[1] += '|## |'
+            rows[2] += '|###|'
+            rows[3] += '|_##|'
+        else:
+            # Print the card's front:
+            rank, suit = card   # The card is a tuple data structure.
+            rows[1] += '|{} |'.format(rank.ljust(2))
+            rows[2] += '| {} |'format('suit')
+            rows[3] += '|_{}|'
