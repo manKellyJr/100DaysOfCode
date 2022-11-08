69a797e8d00c807474259a231d086d7e129fb6cf
Obed Banda <obedbanda34@gmail.com>
2022-11-08 22:17:14 +0200

    Digital Stream commit

---
+++ round_one/039/digitalstream.py
@@ -0,0 +1,15 @@
+"""Digital Stream, by Obed banda
+A scrrensaver in the style of The Matrix movie's visuals.
+Tags: tiny, artistic, beginner, scrolling
+"""
+
+import random, shutil, sys, time
+
+# Set up the constants
+MIN_STREAM_LENGTH = 6
+MAX_STREAM_LENGTH = 14
+PAUSE = 0.1
+STREAM_CHARS = ['0', '1']
+
+# Density can range from 0.0 to 1.0
+
